2b2b4302a9d094b75fa5c39234ab1663a34687ba
vineet patnaik <vineet1.patnaik97@gmail.com>
2024-11-28 16:08:28 -0500

    Update doctor_patient_conversation_decode.py

--- doctor_patient_conversation_decode.py
+++ doctor_patient_conversation_decode.py
@@ -8,20 +8,14 @@ from io import BytesIO
 from groq import Groq 
 
 # Set up proxies (if required)
-#os.environ["HTTP_PROXY"] = "http://proxy_url:proxy_port"
-#os.environ["HTTPS_PROXY"] = "http://proxy_url:proxy_port"
+os.environ["HTTP_PROXY"] = "http://proxy_url:proxy_port"
+os.environ["HTTPS_PROXY"] = "http://proxy_url:proxy_port"
 
 #api_key = st.secrets["API_KEY"]
 
-# Fetch the API key from Streamlit Secrets
-api_key = st.secrets["API_KEY"]
-
-# Initialize Groq API client
-client = Groq(api_key=api_key)
-
 # Initialize Groq API client
 #client = Groq(api_key=api_key)
-#client = Groq(api_key=st.secrets["API_KEY"])
+client = Groq(api_key=st.secrets["API_KEY"])
 
 # Load the Whisper model
 @st.cache_resource
